1c8b3226b171e2d5f7af287b46a03f02bc20f85d
SanCigo <oscar.m.norelli03@gmail.com>
2026-06-17 19:30:29 +0200

    new controller model

--- Scripts/vr_player.gd
+++ Scripts/vr_player.gd
@@ -20,3 +20,35 @@ func _process(delta: float) -> void:
 
 func get_trackers() -> Array[Node3D]:
 	return [$RightController/GestureInputTrackerR, $LeftController/GestureInputTrackerL]
+
+func highlight_button(hand: String, button_name: String, color: Color) -> void:
+	var controller = $RightController if hand == "right" else $LeftController
+	if not controller.has_node("ControllerModel"):
+		return
+	var model_root = controller.get_node("ControllerModel")
+	var button_mesh = _find_mesh_by_name(model_root, button_name.to_lower())
+	if button_mesh:
+		var mat = StandardMaterial3D.new()
+		mat.albedo_color = color
+		mat.emission_enabled = true
+		mat.emission = color
+		mat.emission_energy_multiplier = 2.0
+		button_mesh.set_surface_override_material(0, mat)
+
+func reset_button_highlight(hand: String, button_name: String) -> void:
+	var controller = $RightController if hand == "right" else $LeftController
+	if not controller.has_node("ControllerModel"):
+		return
+	var model_root = controller.get_node("ControllerModel")
+	var button_mesh = _find_mesh_by_name(model_root, button_name.to_lower())
+	if button_mesh:
+		button_mesh.set_surface_override_material(0, null)
+
+func _find_mesh_by_name(node: Node, partial_name: String) -> MeshInstance3D:
+	if node is MeshInstance3D and partial_name in node.name.to_lower():
+		return node as MeshInstance3D
+	for child in node.get_children():
+		var found = _find_mesh_by_name(child, partial_name)
+		if found:
+			return found
+	return null
